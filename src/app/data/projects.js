e38951bc178b3fd69ec8ca214300b34fea661b09
Abdul Al Roman <mdroman45678910@gmail.com>
2025-09-24 15:05:41 +0600

    apply some chane in data

--- src/app/data/projects.js
+++ src/app/data/projects.js
@@ -2,11 +2,12 @@
 export const projects = [
   {
     id: "1",
-    title: "Questly (Product Recommendation)",
+    title: " Product Recommendation",
+    name: "Questly",
     description:
       "An interactive React-based platform where users can post queries, share personalized recommendations, and manage profiles securely with Firebase Authentication.",
     technologiesTitel:
-      "React, Firebase Authentication, Real-time Database, Dynamic Dashboards",
+      "React, Tailwind CSS, Node.js, Express.js, MongoDB, JWT, Axios, React Query, React Hook Form",
     technologies: [
       {
         name: "HTML5",
@@ -45,32 +46,24 @@ export const projects = [
         icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
       },
     ],
-    liveLink: "https://yourprojectlink.com",
-    githubLinkClient: "https://yourprojectlink.com",
-    githubLinkServer: "https://yourprojectlink.com",
+    liveLink: "https://b11-a11.netlify.app/",
+    githubLinkClient: "https://github.com/Abdulal-Roman09/Questly",
+    githubLinkServer:
+      "https://github.com/Abdulal-Roman09/Questly-Product-Recommendation-site-",
     carouselItems: [
-      {
-        img: "https://i.ibb.co/7dmZ3pjX/Screenshot-2025-08-11-071409.png",
-      },
-      {
-        img: "https://i.ibb.co/NnsLzRFq/Screenshot-2025-08-11-071438.png",
-      },
-      {
-        img: "https://i.ibb.co/Mx9j2fqq/Screenshot-2025-08-11-071623.png",
-      },
-      {
-        img: "https://i.ibb.co/kgGffb2K/Screenshot-2025-08-11-071659.png",
-      },
-      {
-        img: "https://i.ibb.co/Kjd4W2b4/Screenshot-2025-08-11-071508.png",
-      },
+      { img: "https://i.ibb.co/7dmZ3pjX/Screenshot-2025-08-11-071409.png" },
+      { img: "https://i.ibb.co/NnsLzRFq/Screenshot-2025-08-11-071438.png" },
+      { img: "https://i.ibb.co/Mx9j2fqq/Screenshot-2025-08-11-071623.png" },
+      { img: "https://i.ibb.co/kgGffb2K/Screenshot-2025-08-11-071659.png" },
+      { img: "https://i.ibb.co/Kjd4W2b4/Screenshot-2025-08-11-071508.png" },
     ],
   },
   {
     id: "2",
-    title: "Hostel mangemnets System",
+    title: "Hostel Management System",
+    name: "Kobi Jashimuddin Hall",
     description:
-      "An interactive React-based platform where users can post queries, share personalized recommendations, and manage profiles securely with Firebase Authentication.",
+      "This system enables students to log in, view and review meals, while administrators can manage student data, meals, and food reviews efficiently — built using the MERN stack.",
     technologies: [
       {
         name: "HTML5",
@@ -109,25 +102,70 @@ export const projects = [
         icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
       },
     ],
-    liveLink: "https://yourprojectlink.com",
-    githubLinkClient: "https://yourprojectlink.com",
-    githubLinkServer: "https://yourprojectlink.com",
+    liveLink: "https://golden-pegasus-af7e4b.netlify.app/",
+    githubLinkClient: "https://github.com/Abdulal-Roman09/Hostel-Management",
+    githubLinkServer:
+      "https://github.com/Abdulal-Roman09/Hostel-Management-Server",
     carouselItems: [
+      { img: "https://i.ibb.co/bRFh6Ws9/Screenshot-2025-08-11-091316.png" },
+      { img: "https://i.ibb.co/kNGBhr5/Screenshot-2025-08-11-091633.png" },
+      { img: "https://i.ibb.co/dwdFbHNy/Screenshot-2025-08-11-091437.png" },
+      { img: "https://i.ibb.co/qMMZbydm/Screenshot-2025-08-11-091530.png" },
+      { img: "https://i.ibb.co/qM300PPn/Screenshot-2025-08-11-091556.png" },
+    ],
+  },
+  {
+    id: "3",
+    title: "Salad Restaurant App",
+    name: "SaladFlavaro",
+    description:
+      "A modern restaurant blog and ordering platform built with Node.js, Express, and MongoDB. Features include comprehensive menu management, customer reviews, and an intuitive ordering system designed to provide an engaging user experience and efficient backend management for restaurant owners.",
+    technologies: [
       {
-        img: "https://i.ibb.co/7dmZ3pjX/Screenshot-2025-08-11-071409.png",
+        name: "HTML5",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
       },
       {
-        img: "https://i.ibb.co/NnsLzRFq/Screenshot-2025-08-11-071438.png",
+        name: "CSS3",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
       },
       {
-        img: "https://i.ibb.co/Mx9j2fqq/Screenshot-2025-08-11-071623.png",
+        name: "JavaScript",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
       },
       {
-        img: "https://i.ibb.co/kgGffb2K/Screenshot-2025-08-11-071659.png",
+        name: "React",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
       },
       {
-        img: "https://i.ibb.co/Kjd4W2b4/Screenshot-2025-08-11-071508.png",
+        name: "Node.js",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
       },
+      {
+        name: "Express",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
+      },
+      {
+        name: "MongoDB",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg",
+      },
+      {
+        name: "Firebase",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg",
+      },
+      {
+        name: "Git",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+      },
+    ],
+    liveLink: "https://whimsical-jelly-bedd51.netlify.app/",
+    githubLinkClient: "https://github.com/Abdulal-Roman09/Salad-Flavor",
+    githubLinkServer: "https://github.com/Abdulal-Roman09/Salad-Flavor-Backend",
+    carouselItems: [
+      { img: "https://i.ibb.co/DDRsZwKL/Screenshot-2025-08-12-061426.png" },
+      { img: "https://i.ibb.co/Q3w7ptLY/Screenshot-2025-08-12-061447.png" },
+      { img: "https://i.ibb.co/ksy0nq5G/Screenshot-2025-08-12-061356.png" },
+      { img: "https://i.ibb.co/JjqSmKs1/Screenshot-2025-08-12-062104.png" },
     ],
   },
 ];
